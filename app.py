cfc2c87df2d10a2489a41903096e43e99d8dc994
Saddam Hossain <sksaddamh08@gmail.com>
2024-09-27 15:10:18 +0530

    implement deepgram first if not exists the yt_dlp output

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from deepgram import Deepgram
 import asyncio
 import threading
 import requests
+import json
+import re
 
 app = Flask(__name__)
 
@@ -22,13 +24,19 @@ def get_cookies():
 
 
 async def transcribe_audio(audio_file):
-    deepgram = Deepgram(DEEPGRAM_API_KEY)
+    try:
+        deepgram = Deepgram(DEEPGRAM_API_KEY)
 
-    with open(audio_file, 'rb') as audio:
-        source = {'buffer': audio, 'mimetype': 'audio/mp3'}
-        response = await deepgram.transcription.prerecorded(source, {'detect_language': True})
+        with open(audio_file, 'rb') as audio:
+            source = {'buffer': audio, 'mimetype': 'audio/mp3'}
+            response = await deepgram.transcription.prerecorded(source, {'detect_language': True})
 
-    return response
+        results = response['results']
+        transcript = results['channels'][0]['alternatives'][0]['transcript']
+
+        return {"results": results, "transcript": transcript}
+    except:
+        return {"transcript": ''}
 
 
 def download_video(url):
@@ -41,24 +49,75 @@ def download_video(url):
             'preferredquality': '192',
         }],
         'outtmpl': '%(title)s.%(ext)s',
-        'cookiefile': cookie_file,
-        'verbose': True,
+        'cookiefile': cookie_file
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         audio_file = ydl.prepare_filename(info).rsplit('.', 1)[0] + '.mp3'
-        return audio_file
+        return audio_file, info
+
+
+def get_yt_dlp_transcript(info):
+    cookie_file = get_cookies()
+    ydl_opts = {
+        'writesubtitles': True,
+        'writeautomaticsub': True,
+        'subtitleslangs': ['all'],
+        'skip_download': True,
+        'outtmpl': 'subtitles',
+        'cookiefile': cookie_file
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        if 'subtitles' in info and info['subtitles']:
+            subtitle_lang = next(iter(info['subtitles']))
+            subtitle_url = info['subtitles'][subtitle_lang][0]['url']
+        elif 'automatic_captions' in info and 'en' in info['automatic_captions']:
+            subtitle_lang = next(iter(info['automatic_captions']))
+            subtitle_url = info['automatic_captions'][subtitle_lang][0]['url']
+        else:
+            return None
+
+        subtitle_data = ydl.urlopen(subtitle_url).read().decode('utf-8')
+        return subtitle_data
 
 
 def process_video_and_transcribe(video_url, callback_url, note_id):
     try:
-        audio_file = download_video(video_url)
+        print(f'Getting transcription for {video_url}')
+
+        audio_file, info = download_video(video_url)
 
         if audio_file and os.path.exists(audio_file):
             transcription = asyncio.run(transcribe_audio(audio_file))
-            results = transcription['results']
-            transcript = results['channels'][0]['alternatives'][0]['transcript']
+            transcript = transcription['transcript']
+
+            # Check if Deepgram transcription is empty
+            if not transcript.strip():
+                print(f'Transcript not found from deepgram for {video_url}')
+
+                # Fallback to yt-dlp transcription
+                transcription = get_yt_dlp_transcript(info)
+
+                if transcription:
+                    results = json.loads(transcription)['events']
+
+                    utf8_strings = []
+                    for event in results:
+                        segs = event.get('segs', [])
+                        utf8_string = segs[0].get('utf8', '')
+                        if utf8_string:
+                            utf8_string = utf8_string.replace('\n', ' ')
+                            utf8_string = re.sub(r'(\\"|")', '', utf8_string)
+                            utf8_strings.append(utf8_string)
+
+                    transcript = ' '.join(utf8_strings)
+                else:
+                    raise Exception(
+                        "Both Deepgram and yt-dlp failed to provide transcription")
+            else:
+                results = transcription['results']
 
             # Delete the audio file
             os.remove(audio_file)
@@ -70,6 +129,7 @@ def process_video_and_transcribe(video_url, callback_url, note_id):
             requests.post(callback_url, json={
                           "note_id": note_id, "error": "Failed to download audio"})
     except Exception as e:
+        print(e)
         requests.post(callback_url, json={"note_id": note_id, "error": str(e)})
 
 
